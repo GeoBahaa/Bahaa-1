cac5a0f3404ddedb3f68106ef132ace8a129fa4d
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-08-15 23:35:45 -0600

    update

--- SandboxPOS/src/MainPanel.java
+++ SandboxPOS/src/MainPanel.java
@@ -10,28 +10,27 @@ public class MainPanel extends JPanel{
 	MonitorBox monitorBox;
 	TransBox transBox;
 	
-	private JPanel bottomPanel = new JPanel();
-	private JPanel monitorPanel;
+	private JPanel inputBox = new JPanel();
 	
 	public MainPanel() {
 		monitorBox = new MonitorBox (this);
 		transBox = new TransBox(this);
 		JPanel mPanel = new JPanel(new BorderLayout());
 	      mPanel.add(monitorBox, BorderLayout.CENTER);
-	      mPanel.add(bottomPanel, BorderLayout.SOUTH);
+	      mPanel.add(inputBox, BorderLayout.SOUTH);
 
 	      setLayout(new BorderLayout());
 	      add(transBox, BorderLayout.EAST);
 	      add(mPanel, BorderLayout.CENTER);
 
 	      transBox.setMinimumSize(new Dimension(150, 0));
-	      bottomPanel.setMinimumSize(new Dimension(0, 100));
+	      inputBox.setMinimumSize(new Dimension(0, 50));
 
 	      transBox.setPreferredSize(transBox.getMinimumSize());
-	      bottomPanel.setPreferredSize(bottomPanel.getMinimumSize());
+	      inputBox.setPreferredSize(inputBox.getMinimumSize());
 
 	      transBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	      bottomPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	      inputBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	      monitorBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
 }
